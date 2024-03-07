1afc779368cf924a8fd2450ff328c527cc7a611f
ahmadkafil70 <133521691+ahmadkafil70@users.noreply.github.com>
2024-03-07 21:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if(n<=2) return n-1;
+	let a=0, b=1;
+	while (n>2) {
+		let curr=a+b;
+		a=b;
+		b=curr;
+		n--;
+		
+	}
 }
 
 module.exports = fibonacci;
